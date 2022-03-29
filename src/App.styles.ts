ad6ee93cf11c8dc86b501c6a4266aacbf22d3ad9
EvandroSantana <evandromsantana@hotmail.com>
2022-03-29 15:37:58 -0300

    primeiro commit rpg

---
+++ src/App.styles.ts
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+    background-color: #24282F;
+    min-height: 100vh;
+    color: #FFFFFF;
+`;
+
+export const Map = styled.div`
+    width: 900px;
+    height: 660px;
+    background-image: url('/assets/map2.png');
+    background-position: left top;
+    background-size: 100%;
+`;
